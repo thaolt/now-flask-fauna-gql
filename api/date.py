3b72d60943cac3ca48c97e619d2769ece1a134cc
Thao le <thaolt@songphi.com>
2020-02-29 23:45:30 +0700

    test gql with faunadb graphql endpoint

--- api/date.py
+++ api/date.py
@@ -6,6 +6,12 @@ from datetime import datetime
 import os
 from lib.supershop import *
 
+from gql import gql, Client
+from gql.transport.requests import RequestsHTTPTransport
+import json
+
+
+
 load_env()
 
 class handler(BaseHTTPRequestHandler):
@@ -13,9 +19,29 @@ class handler(BaseHTTPRequestHandler):
   def do_GET(self):
     
     self.send_response(200)
-    self.send_header('Content-type', 'text/plain')
+    self.send_header('Content-type', 'application/json')
     self.end_headers()
-    # self.wfile.write(str(datetime.now().strftime('%Y-%m-%d %H:%M:%S')).encode())
-    self.wfile.write(os.getenv("FAUNADB_API_KEY").encode())
+
+    client = Client(
+      transport=RequestsHTTPTransport(
+        url='https://graphql.fauna.com/graphql', 
+        headers={'Authorization': 'Bearer ' + os.getenv("FAUNADB_API_KEY")},
+        use_json=True,
+      ),
+      fetch_schema_from_transport=True,
+    )
+
+    query = gql("""
+    {
+      findClientByID(id: "258719855966945812") {
+        email
+      }
+    }
+    """)
+
+    self.wfile.write(
+      json.dumps(client.execute(query)).encode('utf-8')
+    )
+
     return
 
