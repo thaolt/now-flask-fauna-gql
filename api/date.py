1df412a556aed4ad6f2ddf195b88f3f8aeb81415
Thao le <thaolt@songphi.com>
2020-02-29 18:46:48 +0700

    getenv test

--- api/date.py
+++ api/date.py
@@ -1,5 +1,10 @@
 from http.server import BaseHTTPRequestHandler
 from datetime import datetime
+from dotenv import load_dotenv
+import os
+from pathlib import Path  # python3 only
+env_path = Path('..') / '.env'
+load_dotenv(dotenv_path=env_path)
 
 class handler(BaseHTTPRequestHandler):
 
@@ -7,6 +12,7 @@ class handler(BaseHTTPRequestHandler):
     self.send_response(200)
     self.send_header('Content-type', 'text/plain')
     self.end_headers()
-    self.wfile.write(str(datetime.now().strftime('%Y-%m-%d %H:%M:%S')).encode())
+    # self.wfile.write(str(datetime.now().strftime('%Y-%m-%d %H:%M:%S')).encode())
+    self.wfile.write(os.getenv("FAUNADB_API_KEY").encode())
     return
 
